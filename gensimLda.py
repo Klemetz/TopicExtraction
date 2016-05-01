b51ee3c3a9393877516b2fc759925f7d25d3a53c
Jonathan <jonathanklemetz@gmail.com>
2016-05-01 13:36:54 +0200

    Added multi core and tested on a large sample.

--- gensimLda.py
+++ gensimLda.py
@@ -1,4 +1,4 @@
-import gensim, os, bz2
+import gensim, os, bz2, inspect
 from gensim import corpora, models, similarities
 import logging
 
@@ -22,18 +22,29 @@ class MyCorpus(object):
             yield self.dictionary.doc2bow(tokens)
 
 def main():
-	top_directory = '/Users/jonathanklemetz/Desktop/papersText'
-	logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
-	corpus = MyCorpus(top_directory) #Creates a MyCorpus object, containing all the documents
-	dictionary = corpora.Dictionary.load('/tmp/deerwester.dict')
+    top_directory = '/Users/jonathanklemetz/Desktop/Lemmatized'
+    logging.basicConfig(format='%(asctime)s : %(levelname)s : %(message)s', level=logging.INFO)
+    corpus = MyCorpus(top_directory) #Creates a MyCorpus object, containing all the documents
+    dictionary = corpora.Dictionary.load('/tmp/deerwester.dict')
 
-	# Not entierly sure what this is doing, but it was necessary to create a proper corpus object.
-	corpora.MmCorpus.serialize('/tmp/corpus.mm', corpus)
-	corpus = corpora.MmCorpus('/tmp/corpus.mm')
-	print(corpus)
+    # Not entierly sure what this is doing, but it was necessary to create a proper corpus object.
+    # I think this is because that gensims algorithms require to have the corpuses stored on the hard drive.
+    corpora.MmCorpus.serialize('/tmp/corpus.mm', corpus)
+    corpus = corpora.MmCorpus('/tmp/corpus.mm')
+    print(corpus)
 
-	lda = gensim.models.ldamodel.LdaModel(corpus=corpus, id2word=dictionary, num_topics=50, passes=20)
-	lda.print_topics(50)
+    #lda = gensim.models.ldamodel.LdaModel(corpus=corpus, id2word=dictionary, num_topics=5, passes=10)
+    lda = gensim.models.ldamulticore.LdaMulticore(corpus=corpus, id2word=dictionary, num_topics=200, passes=5, batch=True, workers=2, chunksize=3000, iterations=100)
+    #lda.log_perplexity(chunk=chunksize, total_docs=2163)
+    lda.print_topics(10, num_words=10)
+
+    document = corpus[0]
+
+
+#for document in corpus:
+#printTopic = lda.get_document_topics(document, minimum_probability=None)
+#print(printTopic)        
+#print(document[1])
 
 
 main()
