beab380f64ee207e43fa3772d46647f2504f0353
KlasMagnus <magnusjohansson82@gmail.com>
2016-05-18 17:50:09 +0200

    added fig to stopwords

--- token_lemma_removereferences.py
+++ token_lemma_removereferences.py
@@ -15,7 +15,7 @@ stopWords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you',
 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here',
 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more',
 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so',
-'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now']
+'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now', 'fig']
 
 currentFile = open(sys.argv[1])
 
