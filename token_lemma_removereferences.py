5701db5f63340ea52a7601809ea3fba256386191
KlasMagnus <magnusjohansson82@gmail.com>
2016-05-18 11:49:36 +0200

    updated text-cleaner

--- token_lemma_removereferences.py
+++ token_lemma_removereferences.py
@@ -1,49 +1,91 @@
-from nltk import word_tokenize
+from nltk.tokenize import RegexpTokenizer
 from nltk.stem import WordNetLemmatizer
 import nltk.corpus
 import sys
 
-stopWords = nltk.corpus.stopwords
+#stopWords = nltk.corpus.stopwords
+stopWords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours',
+'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers',
+'herself', 'it', 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves',
+'what', 'which', 'who', 'whom', 'this', 'that', 'these', 'those', 'am', 'is', 'are',
+'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does',
+'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until',
+'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into',
+'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down',
+'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here',
+'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more',
+'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so',
+'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', 'should', 'now']
 
-file = open(sys.argv[1])
+currentFile = open(sys.argv[1])
 
 print str(sys.argv[1])
-originalString = file.read()
-originalString = originalString.lower()
-originalString = unicode(originalString, 'utf-8')
+stringToTokenize = currentFile.read()
+#currentFile.close()
+stringToTokenize = stringToTokenize.lower()
+stringToTokenize = unicode(stringToTokenize, 'utf-8')
 
+tokenizer = RegexpTokenizer('[a-z]\w+')
 
-tokenizedString = word_tokenize(originalString)
-print(tokenizedString)
+tokenizedString = tokenizer.tokenize(stringToTokenize)
+#print(tokenizedString)
 
-for word in tokenizedString:
-    if word in stopWords.words('english'):
-        tokenizedString.remove(word)
 
 lemmatizer = WordNetLemmatizer()
 lemmatizedList = [lemmatizer.lemmatize(t) for t in tokenizedString]
 
-print(lemmatizedList)
+counter = 0
+for word in lemmatizedList:
+    if word == 'the':
+        counter = (counter + 1)
+
+print("No of the's: ", counter)
+for word in lemmatizedList:
+    if word in stopWords:
+        print("removing ", word, " from list")
+	#if word in stopWords.words('english'):
+        lemmatizedList.remove(word)
+
+counter2 = 0
+for word in lemmatizedList:
+    if word == 'the':
+        counter2 = (counter2 + 1)
+
+print("No of the's after stopword removal: ", counter2)
+
+counter3 = 0
+for word in lemmatizedList:
+    if word in stopWords:
+        print("removing ", word, " from list")
+        # if word in stopWords.words('english'):
+        lemmatizedList.remove(word)
+print("No of the's after stopword removal 2nd pass xD : ", counter3)
+
+
+#print(lemmatizedList)
 
 """ look through the list, find the last occurence of "reference" (remember s is removed)
 and delete the rest of the list
 
 code goes here....
 """
+
+
 refID = 0
+hasReferences = False
 for index, word in enumerate(lemmatizedList):
     if word == "reference":
-        print(index)
+        # print(index)
+        hasReferences = True
         refID = index
 
-
-print(refID)
-
-finalString = lemmatizedList[: refID]
+finalString = lemmatizedList
+#print(refID)
+if hasReferences:
+    finalString = lemmatizedList[: refID]
 
 
 newFileName = 'lemmatized_' + str(sys.argv[1])
-
 with open(newFileName, 'w') as newFile:
 
     for item in finalString:
